52b65ede8a9eb40d609ee5ad7ca9f6f2716bcf08
Ruben Reyes <rubenreyesdaniel@gmail.com>
2023-06-19 11:54:52 -0600

    Componente Alerta

---
+++ src/components/Alerta.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    alerta: {
+        type: Object,
+        required: true 
+    }
+})
+
+const alertas = {
+    error: () => "bg-red-500",
+    exito: () => "bg-green-500"
+}
+
+const isError = computed(() => {
+    return alertas[props.alerta.tipo]()
+})
+</script>
+
+<template>
+    <div
+    :class="isError"
+    class="text-white text-center p-3 uppercase font-bold mt-3 rounded-md">
+    {{ alerta.mensaje }}</div>
+</template>
